0f9992053370d3ceaa66e0453b2fb83e7a87ec9d
devonnuri <devonnuri@gmail.com>
2019-01-27 00:07:52 +0900

    Show border in PlayScreen

--- imaima.Game/Screens/Play/PlayScreen.cs
+++ imaima.Game/Screens/Play/PlayScreen.cs
@@ -1,6 +1,8 @@
 ﻿using imaima.Game.Containers;
 using imaima.Game.Songs;
 using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Textures;
 using osu.Framework.Screens;
 
@@ -24,6 +26,13 @@ namespace imaima.Game.Screens.Play {
                     textureStore.Get("Frames/gochuumon.png")
                 )
             );
+
+            circularContainer.AddRange(new Drawable[] {
+                new Box {
+                    RelativeSizeAxes = Axes.Both,
+                    Texture = textureStore.Get("Borders/white.png")
+                }
+            });
         }
     }
 }
